50119624c7c5719d9f61c80eef8db5625274d035
Jake o'brien <jacobrien64@gmail.com>
2023-03-09 23:59:40 -0800

    fixed bug

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,29 @@
-#![allow(unused)]
-
-use std::os::unix::prelude;
-use std::{io, num};
-use std::{thread, time};
-
+//use std::{thread, time};
 use rand::Rng;
-use std::io::{Write, BufReader, BufRead, ErrorKind};
-use std::fs::File;
-use std::cmp::Ordering;
 
 fn main() {
     let option_one = std::env::args().nth(1).expect("Option one arguement is missing");
     let option_two = std::env::args().nth(2).expect("Option two arguement is missing");
-    let highest_random_num: i32 = 100001;
-    let wanting_num: i32 = rand::thread_rng().gen_range(0..highest_random_num);
+    let highest_random_num: i32 = 666;
+    let mut wanting_num: i32 = rand::thread_rng().gen_range(0..highest_random_num);
 
     let mut random_num: i32 = rand::thread_rng().gen_range(1..highest_random_num);
 
     println!("rand number test: {}", random_num);
 
-    let mut loop_count: i64 = 1;
+    let mut loop_count: i64 = 0;
     loop {
+        if wanting_num == 0 {
+            wanting_num += 1;
+        }
         random_num = rand::thread_rng().gen_range(1..highest_random_num);
         println!("\nKey: {}", loop_count);
         println!("alpha: {}\nbeta: {}\n", random_num, wanting_num);
 
-        if (random_num == wanting_num) {
+        if random_num == wanting_num {
             println!("Alpha matches with Beta... \ncalculating the key for anaswer...");
             println!("done!");
-            if (is_even(loop_count.into()) == true) {
+            if is_even(loop_count.into()) == true {
                 println!("\n\ngo and {}", option_one);
             }
             else {
@@ -49,7 +44,7 @@ fn main() {
 
 fn is_even(mut num: i64) -> bool {
     num = num % 2;
-    if (num == 0) {
+    if num == 0 {
         return true;
     }
     else {
